7e9ea4f33f7969a9c8c7e4ddcf49c97742c09825
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-09 21:51:07 +0200

    Mobile friendly

---
+++ public/publicKey.js
@@ -0,0 +1,8 @@
+window.publicKeyJwk = {
+  alg: 'RSA-OAEP-256',
+  e: 'AQAB',
+  ext: true,
+  kty: 'RSA',
+  n:
+    'ysUeTGwQ8ef2iyppRS99-muj2-9BaUYUFmhGzq0E8CvDihKICKOEagoU2MjlwUWFiYR3KS3tsVVX3jKL95B7fuqUNRcEmES8iYndf1srLpg-2MC_79pEE9SaxiKBaRXCJ7YGkOTy9wWswpUan6gb25MskWB8LTPJMAaaRoz026g8G1vMlxQsT07fsH3IjGJ56tiIW8vYE-UNZ0X6jIHUYuUrrSqkMJpn8OP4EmKJ0faoROEmG-3gWUYxO6RUbopDV5tte_cjsqo-D8mdMLdYQ3VL0mif-w3piCeJiLOnE1oIv4Y8WraD2BzVIFa3dbsLygcurmZc9ij4vgaryml1YQ',
+}
